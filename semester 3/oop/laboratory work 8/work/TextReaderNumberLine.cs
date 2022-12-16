40cac2604ce3d0c1df812ae77473306c0fdf8662
mina987 <44808807+m9studio@users.noreply.github.com>
2022-12-16 20:01:03 +0300

    Add files via upload

---
+++ semester 3/oop/laboratory work 8/work/TextReaderNumberLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    public class TextReaderNumberLine : TextReaderDecorator
+    {
+        private int Line;
+        public TextReaderNumberLine(TextReader component) : base(component) { }
+        public override string ReadLine()
+        {
+            string Text = base.ReadLine();
+            return Text == null ? null : $"{Line++}. {Text}";
+        }
+    }
+}
