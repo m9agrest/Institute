1b112e7c25a1cab15a2ceeb0c831a7d855f3f521
mina987 <44808807+m9studio@users.noreply.github.com>
2022-12-16 19:58:11 +0300

    Add files via upload

---
+++ semester 3/oop/laboratory work 7/laboratory work 8/work/TextWriterNumberLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    public class TextWriterNumberLine : TextWriterDecorator
+    {
+        public TextWriterNumberLine(TextWriter component) : base(component) { }
+        private int Line = 0;
+        public override void WriteLine(string text)
+        {
+            Line++;
+            string NewText = $"{Line}) {text}";
+            base.WriteLine(NewText);
+        }
+    }
+}
