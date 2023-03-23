2f98eb73deed541f0c1dcad408f83784d31668a7
mina987 <erm.555.mmm@gmail.com>
2023-03-23 13:20:23 +0300

    added lab 4 on operating systems

---
+++ semester 4/Operating systems/laboratory work 4/work/Program.cs
@@ -0,0 +1,20 @@
+﻿Thread t1 = new Thread(func1);
+t1.Priority = ThreadPriority.Lowest;
+Thread t2 = new Thread(func2);
+t2.Priority = ThreadPriority.BelowNormal;
+Thread t3 = new Thread(func3);
+t3.Priority = ThreadPriority.Highest;
+
+t1.Start();
+t2.Start();
+t3.Start();
+
+
+void func1() => func(0, 9);
+void func2() => func(10, 19);
+void func3() => func(100, 120);
+void func(int a, int b)
+{
+    for(int i = a; i <= b; i++)
+        Console.Write(i + " ");
+}
