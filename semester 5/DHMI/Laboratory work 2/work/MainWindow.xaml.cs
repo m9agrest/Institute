29208de833c3e68f4d547ecdaa23f79224694a2a
mina987 <erm.555.mmm@gmail.com>
2023-10-22 16:32:50 +0300

    .

--- semester 5/DHMI/Laboratory work 2/work/MainWindow.xaml.cs
+++ semester 5/DHMI/Laboratory work 2/work/MainWindow.xaml.cs
@@ -1,35 +1,133 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace work
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        MD5 md5 = MD5.Create();
+        string HashMd5 = "";
+        string LastFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void DropFiles(object sender, DragEventArgs e)
+        private void DropFile(object sender, DragEventArgs e) => SelectFile(_DropFile(e));
+        private void DropFileMd5(object sender, DragEventArgs e) => SelectFileMd5(_DropFile(e));
+        private void SelectFile(object sender, MouseButtonEventArgs e) => SelectFile(_SelectFile(null));
+        private void SelectFileMd5(object sender, MouseButtonEventArgs e) => SelectFileMd5(_SelectFile("Hash Files (*.md5)|*.md5|All Files (*.*)|*.*"));
+        private string? _DropFile(DragEventArgs e)
         {
-            Debug.WriteLine("+");
-            Console.WriteLine("+");
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if(files.Length > 0)
+                {
+                    return files[0];
+                }
+            }
+            return null;
         }
-
+        private string? _SelectFile(string? Filter)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(Filter != null)
+            {
+                dialog.Filter = Filter;
+            }
+            dialog.InitialDirectory = LastFolder;
+            if (dialog.ShowDialog() == true)
+            {
+                string? lastFolder = Path.GetDirectoryName(dialog.FileName);
+                if(lastFolder != null)
+                {
+                    LastFolder = lastFolder;
+                }
+                return dialog.FileName;
+            }
+            return null;
+        }
+        private void SelectFile(string? src)
+        {
+            if(src != null)
+            {
+                try
+                {
+                    HashMd5 = BitConverter.ToString(md5.ComputeHash(File.ReadAllBytes(src))).Replace("-", "").ToLower();
+                    LabelFileSrc.Content = "Файл: " + src;
+                    LabelFileMd5.Content = "Хэш файла: " + HashMd5;
+                    BuutonOpenMd5.Visibility = Visibility.Visible;
+                    BuutonSaveMd5.Visibility = Visibility.Visible;
+                }
+                catch
+                {
+                    ClearFile(null, null);
+                    MessageBox.Show("При чтении файла, произошла ошибка", "Ошибка");
+                }
+            }
+        }
+        private void SelectFileMd5(string? src)
+        {
+            if (src != null)
+            {
+                try
+                {
+                    StreamReader reader = new System.IO.StreamReader(src);
+                    string text = reader.ReadToEnd();
+                    reader.Close();
+                    LabelFile2Src.Content = "Файл с хэшем: " + src;
+                    LabelFile2Md5.Content = "Хэш в выбранном файле: " + text;
+                    if (HashMd5 == text)
+                    {
+                        LabelResult.Content = "Хэш из файла, равен хэшу открытого файла";
+                    }
+                    else
+                    {
+                        LabelResult.Content = "Хэш из файла, не равен хэшу открытого файла";
+                    }
+                }
+                catch
+                {
+                    LabelFile2Md5.Content = "";
+                    LabelFile2Src.Content = "";
+                    LabelResult.Content = "";
+                    MessageBox.Show("При чтении файла, произошла ошибка", "Ошибка");
+                }
+            }
+        }
+        private void ClearFile(object? sender, MouseButtonEventArgs? e)
+        {
+            BuutonOpenMd5.Visibility = Visibility.Hidden;
+            BuutonSaveMd5.Visibility = Visibility.Hidden;
+            LabelFileMd5.Content = "";
+            LabelResult.Content = "";
+            LabelFile2Md5.Content = "";
+            LabelFile2Src.Content = "";
+            LabelFileSrc.Content = "Файл не выбран";
+        }
+        private void SaveFile(object sender, MouseButtonEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Hash Files (*.md5)|*.md5|All Files (*.*)|*.*";
+            dialog.InitialDirectory = LastFolder;
+            if (dialog.ShowDialog() == true)
+            {
+                string? lastFolder = Path.GetDirectoryName(dialog.FileName);
+                if (lastFolder != null)
+                {
+                    LastFolder = lastFolder;
+                }
+                string filePath = dialog.FileName;
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.Write(HashMd5);
+                }
+            }
+        }
     }
 }
